59238843c01851b0ebfbb7971f28bf8a8c90470f
Ceki Gulcu <ceki@qos.ch>
2009-09-01 17:50:25 +0200

    - cosmetic changes

--- cal10n-site/src/site/pages/templates/header.js
+++ cal10n-site/src/site/pages/templates/header.js
@@ -1,7 +1,7 @@
 
 document.write('<p align="left">');
 document.write('<a href="http://cal10n.qos.ch/">');
-document.write('<img src="' + prefix + 'images/CAL10N_logo.gif" alt="" border="0" style="margin-left: 4px; " />');
+document.write('<img src="' + prefix + 'images/CAL10N_logo.png" alt="" border="0" style="margin-left: 4px; " />');
 document.write('</a>')
 document.write('</p>');
 document.write('<div id="headerLine"></div>');
