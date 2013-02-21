630b95aa310bda31ccf02f50dd5627b7de8cb90f
Ceki Gulcu <ceki@qos.ch>
2013-02-21 17:20:38 +0100

    update css files, work on next version

--- cal10n-site/src/site/pages/templates/left.js
+++ cal10n-site/src/site/pages/templates/left.js
@@ -6,8 +6,8 @@ document.write('  <p class="menu"><a href="' + prefix + 'news.html">News</a></p>
 document.write('  <p class="menu"><a href="' + prefix + 'download.html">Download</a></p>');
 document.write('  <p class="menu"><a href="' + prefix + 'documentation.html">Documentation</a></p>');
 document.write('  <p class="menu"><a href="' + prefix + 'mailinglist.html">Mailing Lists</a></p>');
-document.write('  <p class="menu"><a href="' + prefix + 'repos.html">Source Repository</a></p>');
 document.write('  <p class="menu"><a href="' + prefix + 'bugreport.html">Bug Report</a></p>');
+document.write('<p class="menu"><a href="http://github.com/qos-ch/cal10n">Source Repository</a></p>');
 document.write('  <p class="menu"><a href="' + prefix + 'license.html">License</a></p>');
 document.write('</div>');
 
